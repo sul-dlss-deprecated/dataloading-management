93fcc6d99251089982b8496ff296cab7c2cb61e6
Michael J. Giarlo <mjgiarlo@stanford.edu>
2023-04-05 10:47:33 -0700

    Add authentication to vendors controller
    Fixes #58
    
    This commit contains more work that initially expected as I worked in the UI to ensure authentication was working for vendor interactions. This led to other bits of work:
    
    * Make a clearer delineation between authentication-related controllers (`LoginController`), UI controllers, and API controllers (in a separate PR)
    * Use REMOTE_USER in `bin/dev` to handle authentication in local dev (and add to README)
    * Rename stats controller (+ views + route) for consistency with other controllers, now that that list has grown
    * Wire up basic vendor interactions in UI for easier testing (both in browser and in RSpec)
    * Use the `folio_id` param of vendors as the ID in the route (which the controller was only doing, and which I only found once the views were written up)

--- spec/requests/homepage_spec.rb
+++ spec/requests/homepage_spec.rb
@@ -1,30 +1,28 @@
 require "rails_helper"
 
-RSpec.describe "Data Loading Management" do
-  describe "/" do
-    context "with an unauthenticated user" do
-      before { sign_out }
+RSpec.describe "/" do
+  context "with an unauthenticated user" do
+    before { sign_out }
 
-      it "renders the stats index page" do
-        get "/"
-        expect(response).to have_http_status(:found)
-        expect(response).to redirect_to(new_user_session_path)
-      end
+    it "renders the homepage" do
+      get "/"
+      expect(response).to have_http_status(:found)
+      expect(response).to redirect_to(new_user_session_path)
     end
+  end
 
-    context "with an authenticated user" do
-      let(:user) { create(:user) }
+  context "with an authenticated user" do
+    let(:user) { create(:user) }
 
-      before { sign_in user }
+    before { sign_in user }
 
-      it "renders the stats index page" do
-        get "/"
-        expect(response).to have_http_status(:ok)
-        expect(response.body).to match(/Data Loading Management Statistics/)
-        expect(response.body).to match(/Status/)
-        expect(response.body).to match(/Vendors/)
-        expect(response.body).to match(/Schedule/)
-      end
+    it "renders the homepage" do
+      get "/"
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to match(/Data Loading Management Statistics/)
+      expect(response.body).to match(/Status/)
+      expect(response.body).to match(/Vendors/)
+      expect(response.body).to match(/Schedule/)
     end
   end
 end
